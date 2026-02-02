cbdc7e252c2c25a1e4eea97cbfa2981d94184c61
gitcat456 <okothdenzel65@gmail.com>
2026-02-02 14:30:33 +0300

    Adds a uuid field to the post model

--- posts/models.py
+++ posts/models.py
@@ -1,16 +1,18 @@
+import uuid
 from django.db import models
 
 #tasks : implement slug and uuid
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(min_lenght=2, max_lenght=50)
+    name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
     
 
 class Posts(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='posts')
-    title = models.CharField(max_lenght=50)
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    date_published = models.DateTimeField(auto_add_now=True)
+    date_published = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, blank=True, null=True)
     

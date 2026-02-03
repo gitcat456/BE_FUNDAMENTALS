fdea5496ce3643eeffc16eb6518fc894acb8277a
gitcat456 <okothdenzel65@gmail.com>
2026-02-03 11:27:12 +0300

    slugify author_name for SEO friendly urls

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,19 @@
 import uuid
 from django.db import models
+from django.utils.text import slugify
 
 #tasks : implement slug and uuid
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
+    slug = models.SlugField(blank=True, null=True)
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     bio = models.TextField(blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug: 
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
     
 
 class Posts(models.Model):
@@ -15,4 +22,7 @@ class Posts(models.Model):
     content = models.TextField()
     date_published = models.DateTimeField(auto_now_add=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
     

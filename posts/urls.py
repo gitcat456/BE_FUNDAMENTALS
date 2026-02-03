a3677134c5acdb70e8b0f8adaa00e9993fc83b51
gitcat456 <okothdenzel65@gmail.com>
2026-02-03 13:02:03 +0300

    adds url with id param for filtering

--- posts/urls.py
+++ posts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include,re_path
-from .views import AuthorViewSet, PostViewSet
+from .views import AuthorViewSet, PostViewSet, post_detail
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
-router.register(r'posts', PostViewSet)
+#router.register(r'posts', PostViewSet)
 router.register(r'author', AuthorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('posts/<int:id>/', post_detail)
     
 ]

e468ea55b6e1de8f1266bccd02cc5e826cc376cc
gitcat456 <okothdenzel65@gmail.com>
2026-02-03 11:31:10 +0300

    refactor classes use viewsets to improve maintainability

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Posts
+from .serializers import AuthorSerializer, PostSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Posts.objects.all()
+    serializer_class = PostSerializer
+    
+    def get_queryset(self):
+        queryset = self.queryset
+        search = self.request.query_params.get('search')
+        
+       
+        if search:
+            queryset = queryset.filter(title__icontains=search).filter(content__icontains=search)
+        return queryset
+        
+  
+        

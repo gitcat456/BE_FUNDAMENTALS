966ab002c2cfb9fe4dd69c983228f1bd700fa167
gitcat456 <okothdenzel65@gmail.com>
2026-02-05 08:59:17 +0300

    creats diff posts path for experimenting

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include,re_path
-from .views import AuthorViewSet, PostViewSet, post_detail
+from .views import AuthorViewSet, PostViewSet, post_detail, all_posts
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
@@ -8,6 +8,8 @@ router.register(r'author', AuthorViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('posts/', all_posts),
     path('posts/<int:id>/', post_detail)
+   
     
 ]

cb5b3d1ba09a9638d7c23a674bcbbc2dccac76d3
gitcat456 <okothdenzel65@gmail.com>
2026-02-05 09:01:11 +0300

    adds RequestLoggerMiddleware to the list of middlewares

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'posts.middleware.PostViewLoggerMiddleware',
+    'posts.middleware.RequestLoggerMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [

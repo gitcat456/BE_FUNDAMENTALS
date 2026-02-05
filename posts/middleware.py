8764e6083944754f49c4093e7c026e89ee1a22ee
gitcat456 <okothdenzel65@gmail.com>
2026-02-05 08:58:12 +0300

    creats custom middleware to log request info

--- posts/middleware.py
+++ posts/middleware.py
@@ -1,14 +1,30 @@
-class PostViewLoggerMiddleware:
+import time
+import logging
+
+logger = logging.getLogger("django.request")  
+class RequestLoggerMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        
+        # One-time setup
+
     def __call__(self, request):
-        print("Request just entered middleware")
-        
-        #above runs before the view
+        # Record start time
+        start_time = time.time()
+
+        # Log request info
+        logger.info(f"Incoming request: {request.scheme} {request.method} {request.path}")
+
+        # Call next middleware / view
         response = self.get_response(request)
-        #below runs after the view 
-        
-        if request.path.startswith("/api/"):
-            print(f"Post accessed: {request.path}")
-        return response 
+
+        # Calculate elapsed time
+        duration = time.time() - start_time
+
+        # Log response info
+        logger.info(
+            f"Response: {request.method} {request.path} "
+            f"Status: {response.status_code} "
+            f"Time taken: {duration:.4f} sec"
+        )
+
+        return response

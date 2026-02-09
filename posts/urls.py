88acee7455c33fb07a9f6bf4e246bcff47cd6a62
gitcat456 <okothdenzel65@gmail.com>
2026-02-09 21:37:18 +0300

    creates url path to the FBV for create and listing posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path, include,re_path
-from .views import AuthorViewSet, PostViewSet, post_detail, all_posts
+from .views import AuthorViewSet, PostViewSet, post_detail, all_posts, post_list_create
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
-#router.register(r'posts', PostViewSet)
+router.register(r'posts', PostViewSet)
 router.register(r'author', AuthorViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('posts/', all_posts),
-    path('posts/<int:id>/', post_detail)
-   
-    
+   path('posts/', post_list_create, name = "create_list")
+   #path('', include(router.urls)),
+   # path('posts/<int:id>/', post_detail)
+      
 ]

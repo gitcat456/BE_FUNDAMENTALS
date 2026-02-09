4b6b982dcb81861f611646d9decad0f603b55565
gitcat456 <okothdenzel65@gmail.com>
2026-02-09 22:41:21 +0300

    create url endpints to perfom crud on posts

--- posts/urls.py
+++ posts/urls.py
@@ -7,8 +7,11 @@ router.register(r'posts', PostViewSet)
 router.register(r'author', AuthorViewSet)
 
 urlpatterns = [
-   path('posts/', post_list_create, name = "create_list")
+   path('posts/', post_list_create, name = "create_list"),
+   path('posts/<int:id>/', post_detail),
+   path('posts/<int:id>/edit', post_detail),
+   path('posts/<int:id>/delete', post_detail)
+   
    #path('', include(router.urls)),
-   # path('posts/<int:id>/', post_detail)
       
 ]

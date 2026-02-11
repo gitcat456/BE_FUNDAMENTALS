309f307107238fc3e0906f0ae5837e08d12b868f
gitcat456 <okothdenzel65@gmail.com>
2026-02-11 17:00:59 +0300

    adds a uuid field

--- coredata/models.py
+++ coredata/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid 
 
 class Data(models.Model):
     name = models.CharField(max_length=50)
@@ -6,6 +7,7 @@ class Data(models.Model):
     nationality = models.CharField(max_length=30)
     id_number = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, blank=True, null=True)
     
     def __str__(self):
         return f" Id_no: {self.id_number}  {self.name}"

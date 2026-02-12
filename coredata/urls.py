63fe7ba4fea18aaf8fc56c780ea22f2ed4d443c7
gitcat456 <okothdenzel65@gmail.com>
2026-02-12 12:57:10 +0300

    structures urls to perform CRUD ops on data

--- coredata/urls.py
+++ coredata/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_data
+from .views import list_create_data, data_detail
 
 urlpatterns = [
-    path('data_list/', list_data)
+    path('data_list/', list_create_data),
+    path('data_detail/<uuid:uuid>/', data_detail),
 ]

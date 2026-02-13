44911d2bd45eb6ce308bd81305e7c42f640e6369
gitcat456 <okothdenzel65@gmail.com>
2026-02-13 12:12:06 +0300

    implements routers for the modelviewset class

--- coredata/urls.py
+++ coredata/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from .views import list_create_data, data_detail
+from django.urls import path, include
+from .views import list_create_data, data_detail, DataViewSets
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'model-view-set', DataViewSets)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('data_list/', list_create_data),
     path('data_detail/<uuid:uuid>/', data_detail),
+    
 ]

cb7df27958a323f389e78fa51d9c1c1eacddc5d5
gitcat456 <okothdenzel65@gmail.com>
2026-02-16 11:07:42 +0300

    declare the basic serializer

--- coredata/serializers.py
+++ coredata/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from .models import Data
 
-class DataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Data
-        fields = "__all__"
+class DataSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True) #read_only because DB creates it.
+    name = serializers.CharField(max_length=50)
+    uni = serializers.CharField(max_length=50)
+    nationality = serializers.CharField(max_length=30)
+    id_number = serializers.IntegerField()
+    created_at = serializers.DateTimeField(read_only=True) #bcoz auto genereated
+    uuid = serializers.UUIDField(read_only=True) # model sets it automatically
+
+
+# class DataSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Data
+#         fields = "__all__"

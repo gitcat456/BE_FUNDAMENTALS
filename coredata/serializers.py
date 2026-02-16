bfaa014190dc69d920eb480bd7a2793f85f8a3e4
gitcat456 <okothdenzel65@gmail.com>
2026-02-16 11:29:24 +0300

    logic to create,update and save instances

--- coredata/serializers.py
+++ coredata/serializers.py
@@ -10,6 +10,17 @@ class DataSerializer(serializers.Serializer):
     created_at = serializers.DateTimeField(read_only=True) #bcoz auto genereated
     uuid = serializers.UUIDField(read_only=True) # model sets it automatically
 
+    def create(self, validated_data):
+        return Data.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.uni = validated_data.get("uni", instance.uni)
+        instance.nationality = validated_data.get("nationality", instance.nationality)
+        instance.id_number = validated_data.get("id_number", instance.id_number)
+        instance.save()
+        return instance
+
 
 # class DataSerializer(serializers.ModelSerializer):
 #     class Meta:

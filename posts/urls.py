a89cd6f15c35273d01dd71194f51a85a2aed5af2
gitcat456 <okothdenzel65@gmail.com>
2026-03-11 20:23:21 +0300

    creates post_list route

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include,re_path
-from .views import AuthorViewSet, PostViewSet, post_detail, all_posts, post_list_create, TagViewSet, CommentViewSet
+from .views import AuthorViewSet, PostViewSet, post_detail, all_posts, post_list_create, TagViewSet, CommentViewSet,post_list
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
@@ -9,6 +9,7 @@ router.register(r'tags', TagViewSet)
 router.register(r'comment', CommentViewSet)
 
 urlpatterns = [
+   path('post_list/', post_list, name = "tests"),
    path('posts_list/', post_list_create, name = "create_list"),
    path('posts_fbv/<int:pk>/', post_detail),
    path('posts_fbv/<int:pk>/edit', post_detail),

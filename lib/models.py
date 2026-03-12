befde192c76a6386e13a4ef0115fcbd12cc8e99e
gitcat456 <okothdenzel65@gmail.com>
2026-03-12 20:47:55 +0300

    creates lib models

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    available_copies = models.IntegerField()  # How many can be borrowed
+    
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    membership_active = models.BooleanField(default=True)
+    
+class Loan(models.Model):
+    member = models.ForeignKey(Member, related_name='loans', on_delete=models.CASCADE)
+    borrowed_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    status = models.CharField(
+        max_length=20,
+        choices=[('borrowed', 'Borrowed'), ('returned', 'Returned')],
+        default='borrowed'
+    )
+    
+class LoanItem(models.Model):
+    loan = models.ForeignKey(Loan, related_name='items', on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)

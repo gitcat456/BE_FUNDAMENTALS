158ed6e3fc5536ab091d675bea6cae2836196420
gitcat456 <okothdenzel65@gmail.com>
2026-03-13 14:16:07 +0300

    adds orders route

--- ecom/urls.py
+++ ecom/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .views import order_list, order_create, ProductViewset
+from .views import order_list, order_create, ProductViewset, OrderViewSet, order_detail
 from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register(r'products', ProductViewset)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('orders/', order_list),
+    path('order_list/', order_list),
     path('order_create/', order_create),
-     path('', include(router.urls)),
+    path('order_detail/<int:pk>/', order_detail),
+    path('', include(router.urls)),
 ]

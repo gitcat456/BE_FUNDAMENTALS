65d9807d3c9f62e9e7a3ec85702a4902b86f428c
gitcat456 <okothdenzel65@gmail.com>
2026-03-15 11:59:16 +0300

    routes loans and member endpoints

--- lib/urls.py
+++ lib/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
-from .views import BookViewSet
+from .views import BookViewSet, LoanViewSet, MemberViewSet
 
 router = DefaultRouter()
-router.register('book', BookViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'loans', LoanViewSet)
+router.register(r'member', MemberViewSet)
 
 urlpatterns = [
     path('', include(router.urls))

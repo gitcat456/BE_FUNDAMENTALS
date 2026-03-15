d3b43fb5d62aed60bfdb10821567b8750cd87dac
gitcat456 <okothdenzel65@gmail.com>
2026-03-15 12:00:08 +0300

    creates loanviewset clasee

--- lib/views.py
+++ lib/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Loan, Member
+from .serializers import (
+    BookSerializer,
+    LoanListSerializer,
+    LoanDetailSerializer,
+    LoanCreateSerializer,
+    MemberSerializer
+    ) 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset = Loan.objects.all()
+    
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return LoanListSerializer
+        elif self.action == 'retrieve':
+            return LoanDetailSerializer
+        elif self.action == 'create':
+            return LoanCreateSerializer
+        return LoanListSerializer
+        
+    
+    
 
 

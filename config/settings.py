d764882e7ca8fdcd1dc7682e7e202679cb55da0e
gitcat456 <okothdenzel65@gmail.com>
2026-03-17 10:01:35 +0300

    adds code to print sql queries

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,18 @@ REST_FRAMEWORK = {
     )
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    },
+}

a2b39a4f8f5e93554bb39d3a8f5f58434835bef7
gitcat456 <okothdenzel65@gmail.com>
2026-04-01 18:32:25 +0300

    creates authentication routes

--- lib/urls.py
+++ lib/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
 from .views import BookViewSet, LoanViewSet, MemberViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
@@ -8,5 +9,9 @@ router.register(r'loans', LoanViewSet)
 router.register(r'member', MemberViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('auth/register/', views.register_view),
+    path('auth/login/', views.login_view),
+    path('auth/logout/', views.logout_view),
+    path('auth/me/', views.me_view),
 ]

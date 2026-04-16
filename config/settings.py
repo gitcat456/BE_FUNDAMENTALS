4decf3eff9b4eb30fde6f555b3f746ce5b4ab19e
gitcat456 <okothdenzel65@gmail.com>
2026-04-16 14:24:22 +0300

    adds library app request logger middleware

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'posts.middleware.RequestLoggerMiddleware',
+    'lib.middleware.RequestLoggerMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
